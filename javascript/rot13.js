ce1e04d30e4a392624b6a204acbd6956985e5ab9
Shaun Ho <shaun.ho@mail.utoronto.ca>
2023-08-28 14:47:46 -0400

    Create rot13.js

---
+++ javascript/rot13.js
@@ -0,0 +1,32 @@
+/*
+How can you tell an extrovert from an introvert at NSA?
+Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.
+
+I found this joke on USENET, but the punchline is scrambled. Maybe you can decipher it?
+According to Wikipedia, ROT13 is frequently used to obfuscate jokes on USENET.
+
+For this task you're only supposed to substitute characters. Not spaces, punctuation, numbers, etc.
+
+Test examples:
+
+"EBG13 rknzcyr." -> "ROT13 example."
+
+"This is my first ROT13 excercise!" -> "Guvf vf zl svefg EBG13 rkprepvfr!"
+*/
+
+function rot13(str) {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const cipher = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+  const punchline = str.split('');
+  
+  for (const i in punchline) {
+    const char = punchline[i];
+    const key = alphabet.indexOf(char);
+    
+    if (key >= 0) {
+      punchline[i] = cipher[key];
+    }
+  }
+  
+  return punchline.join('');
+}
